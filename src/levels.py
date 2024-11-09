ffda7bd7ca600a41bb780a263b585f517aa4e263
Luke Sawczak <luke@sawczak.com>
2024-11-08 21:16:12 -0500

    averaging

--- src/levels.py
+++ src/levels.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+from typing import Union
+
 I = 'I'
 R = 'R'
 
@@ -25,8 +28,11 @@ GRADES = dict(map(reversed, LEVELS.items()))
 def is_valid_level(level: str) -> bool:
     return level.upper() in LEVELS
 
-def is_valid_grade(grade: float) -> bool:
-    return 0 <= grade <= 110
+def is_valid_grade(grade: str) -> bool:
+    try:        
+        return 0 <= float(grade) <= 110
+    except:
+        return False
 
 def to_grade(level: str) -> float:
     return LEVELS[level][0]
@@ -37,11 +43,62 @@ def to_level(grade: float) -> str:
         if grade >= thresh[1]:
             return GRADES[thresh]
 
+def any_to_grade(value: str) -> float:
+    if is_valid_level(value):
+        return to_grade(value)
+    elif is_valid_grade(value):
+        return float(value)
+    else:
+        raise TypeError('Value is not a valid grade or level')
+
 def average() -> tuple[str, float]:
-    pass
+    prompt = 'Enter grade or level; optional space + integer weight (default 1); blank to stop: '
+    items = []
+
+    nxt = input(prompt).upper().strip()
+    while nxt:
+        parts = nxt.split()
+
+        try:
+            if len(parts) == 1:
+                val, wt = parts[0], 1
+            elif len(parts) == 2:
+                val, wt = parts[0], int(parts[1])
+            else:
+                raise Exception('Unrecognized input')
+            
+            val = any_to_grade(val)
+            items.append((val, wt))
+
+        except Exception as e:
+            print(repr(e))
+ 
+        nxt = input(prompt).upper().strip()
+
+    if not items:
+        return ['I', 0]
+
+    else:
+        t = 0
+        tw = 0
+
+        for (val, wt) in items:
+            t += val * wt
+            tw += wt
+        a = (t / tw)
+
+        return [to_level(a), a]
 
 def run() -> None:
-    pass
+    print('Only one program exists at present: averaging')
+
+    choice = input('Press Enter to start or Q to quit: ').upper().strip()
+    while choice != 'Q':
+        print()
+        lv, gr = average()
+        print(f'{lv:<3} | {round(gr)}')
+        print('')
+        choice = input('Press Enter to do another or Q to quit: ').upper().strip()
 
 if __name__ == '__main__':
     run()
